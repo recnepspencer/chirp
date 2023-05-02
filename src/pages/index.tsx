c39f8fe647b953de893b32564df354556c41dd9e
recnepspencer <goldenspencerh@gmail.com>
2023-05-02 16:22:48 -0600

    finished post in db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const user = useUser();
 
-  const user = useUser()
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -18,11 +18,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <div>
-        {!user.isSignedIn && <SignInButton />}
-        {!!user.isSignedIn && <SignOutButton/>}
-    </div>
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+          <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
+
+        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
     </>
   );
